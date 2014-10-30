e61febabd53e42dad93f9b4f0da5d0ecbf4c73e8
kkleiven <kkleiven@andrew.cmu.edu>
2014-10-30 11:48:40 -0400

    printers as objects instead of dictionaries

--- jsonTest.py
+++ jsonTest.py
@@ -9,37 +9,69 @@ def getInfo():
         str_info += line
     return str_info
 
-printerDict = dict()
+def getMoreInfo():
+    info = urllib2.urlopen('http://www.cmu.edu/computing/clusters/printing/locations.html')
+    for line in info:
+        print line
 
-def name(msg):
-    start = msg.find('-')
-    if start == -1:
-        return "name error"
-    else:
-        return msg[start+2:]
+#getMoreInfo()
 
-def status(msg):
-    if msg == "go.gif":
-        status = "available"
-    elif msg == "yield.gif":
-        status = "warning"
-    elif msg == "stop.gif":
-        status = "not working"
-    else:
-        status = "unknown"
-    return status
+printerList = []
+
+class Printer(object):
+    def __init__(self, name):
+        start = name.find('-')
+        if start == -1:
+            self.name = "Name Error"
+        else:
+            self.name = name[start+2:]
+        self.status = None
+        self.error = None
+        self.icon = None
+
+    def __repr__(self):
+        return "%s: %s (%s)" % (self.name, self.icon, self.error)
+        
+    def setIcon(self, msg):
+        if msg == "go.gif":
+            status = "available"
+        elif msg == "yield.gif":
+            status = "warning"
+        elif msg == "stop.gif":
+            status = "not working"
+        else:
+            status = "unknown"
+        self.icon = status
+    def getIcon(self):
+        return self.icon
+    
+    def setStatus(self, msg):
+        self.status = msg
+    def getStatus(self):
+        return self.status
+
+    def setErrorMessage(self, msg):
+        self.error = msg
+    def getErrorMessage(self):
+        return self.error
+
 
 class MyDecoder(json.JSONDecoder):
     def __init__(self):
         json.JSONDecoder.__init__(self, object_hook=self.dict_to_object)
 
     def dict_to_object(self, d):
-        if 'name' in d and 'icon' in d:
-            printer_name = name(d.pop('name'))
-            printer_status = status(d.pop('icon'))
-            printerDict[printer_name] = printer_status
+        if 'name' in d:
+            printer = Printer(d.pop('name'))
+            if 'ready' in d:
+                printer.setStatus(d.pop('ready'))
+            if 'icon' in d:
+                printer.setIcon(d.pop('icon'))
+            if 'status' in d:
+                printer.setErrorMessage(d.pop('status'))
+            printerList.append(printer)
 
 encoded_object = getInfo()
 myobj_instance = MyDecoder().decode(encoded_object)
-for printer in printerDict:
-    print printer, printerDict[printer]
+for printer in printerList:
+    print printer
